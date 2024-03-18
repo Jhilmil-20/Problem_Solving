55f34cb589c5c48b3f0d79dd60012b4ee198d43e
Jhilmil-20 <111375848+Jhilmil-20@users.noreply.github.com>
2024-03-19 00:00:16 +0530

    Median of Two Sorted Arrays.java

--- Searching_5/Median of Two Sorted Arrays.java
+++ Searching_5/Median of Two Sorted Arrays.java
@@ -20,5 +20,58 @@ class Solution {
     }
 }
 
-//optimised 
+//optimised , using binary search 
+//The solution is not fully correct , but I tried. 
+class Solution {
+    public int count(int[] arr, int n, int key) {
+        int left = 0, right = n;
+
+        int mid = 0;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] == key) {
+                while (mid + 1 < n && arr[mid + 1] == key)
+                    mid++;
+            } else if (arr[mid] > key)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        while (mid > -1 && arr[mid] > key)
+            mid--;
+        return mid + 1;
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int low = Math.min(nums1[0], nums2[0]);
+        int high = Math.max(nums1[n - 1], nums2[m - 1]);
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int countlessthanmid = count(nums1, n, mid) + count(nums2, m, mid);
+
+            if (countlessthanmid < (n + m + 1) / 2) {
+                low = mid + 1;
+            } else if (countlessthanmid > (n + m + 1) / 2){
+                high = mid - 1;
+            }
+            else{
+                ans = mid;
+                low =mid+1;
+            }
+        }
+
+        if ((n + m) % 2 == 0) {
+            int next = low;
+            while (count(nums1, n, next) + count(nums2, m, next) == (n + m) / 2) {
+                next++;
+            }
+            ans = (ans + next) / 2;
+        }
+        return ans;
+    }
+}
 
