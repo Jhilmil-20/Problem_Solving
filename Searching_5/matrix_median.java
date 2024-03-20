94a1cdce07ea8ae81c1eeb61bab00f429c0d5574
Jhilmil-20 <111375848+Jhilmil-20@users.noreply.github.com>
2024-03-21 00:01:17 +0530

    Initial commit

---
+++ Searching_5/matrix_median.java
@@ -0,0 +1,33 @@
+public static int matrix(int[] arr,int mid){
+	    int low=0;
+	    int high=arr.length-1;
+	    while(low<=high){
+	      int mid1=(low+high)/2;
+	      if(arr[mid1]<=mid){
+	        low=mid+1;
+	      }else{
+	        high=mid-1;
+	      }
+	    }
+	    return low;
+	  }
+
+	public static int findMedian(int[][] arr){
+	  int low=1;
+	  int high=Integer.MAX_VALUE;
+	  int n=arr.length;
+	  int m=arr[0].length;
+	  while(low<=high){
+	    int mid=low+(high-low)/2;
+	    int c=0;
+	    for(int i=0;i<n;i++){
+	      c+=matrix(arr[i],mid);
+	    }
+	    if(c<=(n*m)/2){
+	      low=mid+1;
+	    }else{
+	      high=mid-1;
+	    }
+	  }
+	  return low;
+	}
