9bc8dba264b1f0adcc969e0879beb26224cc6a7b
Jhilmil-20 <111375848+Jhilmil-20@users.noreply.github.com>
2024-03-21 00:00:14 +0530

    Create Search a 2D matrix.java

---
+++ Searching_5/Search a 2D matrix.java
@@ -0,0 +1,21 @@
+public class Search_a_2D_matrix {
+	 public static boolean search(int[][] arr,int target){
+		    int n=arr.length;
+		    int m=arr[0].length;
+		    int low=0;
+		    int high=(n*m)-1;
+		    while(low<=high){
+		      int mid=low+(high-low)/2;
+		      if(arr[mid/m][mid%m]==target){
+		        return true;
+		      }
+		      else if(arr[mid/m][mid%m]>target){
+		        high=mid-1;
+		      }
+		      else{
+		        low=mid+1;
+		      }
+		    }
+		    return false;
+		  }
+}
